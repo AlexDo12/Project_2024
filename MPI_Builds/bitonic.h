9bad5384ac5dab10b02043bd83e6f5ac4fb2d5cb
AlexDo12 <alexthanhdo2003@gmail.com>
2024-10-16 19:09:52 -0500

    Readd bitonic sort implementation

--- MPI_Builds/bitonic.h
+++ MPI_Builds/bitonic.h
@@ -1,6 +1,21 @@
 #ifndef BITONIC_H
 #define BITONIC_H
 
-void bitonic(); // Declaration of the mergesort function
+#include <vector>
+#include "mpi.h"
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
+
+using std::vector;
+
+#define MASTER 0
+
+void bitonic(vector<int>& random);
+void compareExchange(std::vector<int>& local_array, int partner, int rank, int elements_per_process, bool ascending);
 
 #endif
